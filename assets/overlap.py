76cbb35acb6a96ac8d23301ceb7136fddb92ad94
Yuki Iwabuchi <8ucchiman@gmail.com>
2024-03-03 17:17:49 +0900

    update tracking

--- assets/overlap.py
+++ assets/overlap.py
@@ -1,13 +1,17 @@
+import sys
 import cv2 as cv
 
 
 def main():
-    img_arm_tracking = cv.imread("arm_tracker/arm_tracking.jpg", 0)
+    tracking_path = sys.argv[1]
+    fuji_path = sys.argv[2]
+    #img_arm_tracking = cv.imread("arm_tracker/arm_tracking.jpg", 0)
+    img_arm_tracking = cv.imread(tracking_path, 0)
     ret, mask = cv.threshold(img_arm_tracking, 0, 255, cv.THRESH_OTSU)
     #mask_inv = cv.bitwise_not(mask)
     #cv.imwrite("mask.jpg", mask)
 
-    img_fuji = cv.imread("processing_images/resize_bin_fuji_01.jpg")
+    img_fuji = cv.imread(fuji_path)
     overlap = cv.bitwise_and(img_fuji, img_fuji, mask=mask)
     #overlap_bg_src = cv.cvtColor(overlap, cv.COLOR_BGR2RGB)
     #blend = cv.addWeighted(img_fuji, 0.5, img_arm_tracking, 0.5, 0)
