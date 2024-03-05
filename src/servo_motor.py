81acbe700bf8abd7c099ce936b57423756054fc9
Yuki Iwabuchi <8ucchiman@gmail.com>
2024-03-05 13:34:43 +0900

    add render area

--- src/servo_motor.py
+++ src/servo_motor.py
@@ -1,15 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# FileName:     servo_morot
+# Author:       8ucchiman
+# CreatedDate:  2024-02-24 10:52:54
+# LastModified: 2024-03-05 10:54:22
+# Reference:    8ucchiman.jp
+# Description:  ---
+#
+
 import Adafruit_PCA9685
 from time import sleep
 
-class ServoMotor:
 
+class ServoMotor:
     def __init__(self, Channel, ZeroOffset):
         self.mChannel = Channel
         self.m_ZeroOffset = ZeroOffset
 
         #initialize PCA9685
         self.mPwm = Adafruit_PCA9685.PCA9685(address=0x40) 
-        self.mPwm.set_pwm_freq(60) # 60Hz
+        self.mPwm.set_pwm_freq(60)  # 60Hz
 
     def setAngle(self, angle):
         pulse = int((650-150)*angle/180+150+self.m_ZeroOffset)
@@ -21,7 +32,3 @@ class ServoMotor:
         sleep(1)
         self.mPwm.set_pwm(self.mChannel, 0, 0)
         sleep(1)
-
-
-    #def check_angle(self):
-    #    self.#
